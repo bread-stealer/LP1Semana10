32f8a82827558821dd9b14993511137aa758bc73
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-13 12:50:08 +0100

    Add foreach logic for each collections

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
@@ -6,7 +7,23 @@ namespace IntCollections
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int[] values = {1, 10, -30, 10, -5};
+
+            List<int> list = new List<int>();
+            foreach (int v in values) list.Add(v);
+            Console.WriteLine("List: " + string.Join(", ", list));
+
+            Stack<int> stack = new Stack<int>();
+            foreach (int v in values) stack.Push(v);
+            Console.WriteLine("Stack: " + string.Join(", ", stack));
+
+            Queue<int> queue = new Queue<int>();
+            foreach (int v in values) queue.Enqueue(v);
+            Console.WriteLine("Queue: " + string.Join(", ", queue));
+
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (int v in values) hashSet.Add(v);
+            Console.WriteLine("HashSet: " + string.Join(", ", hashSet));
         }
     }
 }
