eb9afdc64df7d34015da5cf6314f5c03389e3694
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-13 13:21:53 +0100

    Add Main Logic to print Player Classes

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EqualPlayer
 {
@@ -6,7 +7,17 @@ namespace EqualPlayer
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
+            setOfPlayers.Add(new Player(PlayerClass.Slayer, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerClass.Tank, "Ana"));
+            //setOfPlayers.Add(new Player(PlayerClass.Mage, "Paulo"));
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
         }
     }
 }
