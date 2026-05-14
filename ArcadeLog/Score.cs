d1ef5b1ec136cf3e711378818b17f0f0fb4066d9
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-14 15:59:20 +0100

    Replace with Moodle Files

---
+++ ArcadeLog/Score.cs
@@ -0,0 +1,32 @@
+namespace ArcadeLog
+{
+    public class Score : IComparable<Score>
+    {
+        // Variável de Instância Privada: points (int)
+        // CÓDIGO AQUI
+
+        // Propriedade Auto-Implementada Só de Leitura: Name (string)
+        // CÓDIGO AQUI
+
+        // Propriedade: Points (int), sempre entre 0 e 9999
+        // CÓDIGO AQUI
+
+        // Propriedade Só de Leitura: Medal (string)
+        // CÓDIGO AQUI
+
+        // Construtor: aceita nome e pontuação
+        // CÓDIGO AQUI
+
+        public int CompareTo(Score other)
+        {
+            // CÓDIGO AQUI
+            return 0; // substitua o return
+        }
+
+        public override string ToString()
+        {
+            // CÓDIGO AQUI
+            return ""; // substitua o return
+        }
+    }
+}
