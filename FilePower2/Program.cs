facac1e033518b8d4640aada2528b10910d6e6da
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-14 14:42:47 +0100

    Add FilePower2 Program using StreamWiter

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FilePower2
 {
@@ -6,7 +7,27 @@ namespace FilePower2
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Provide a filename as arg.");
+                return;
+            }
+
+            string filename = args[0];
+
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                Console.Write("Enter lines of text: ");
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "") break;
+                    sw.WriteLine(input);
+                }
+
+                Console.WriteLine($"Saved to {filename}.");
+            }
         }
     }
 }
