54d07244e53c87a79493d6f35bff86bc55fcfdc3
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-05-14 14:18:42 +0100

    Add FilePower1 to solution

---
+++ FilePower1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
